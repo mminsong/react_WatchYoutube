ebbc42f4c64f500b67c61b7da672ac633816916e
mminsong <pamten@naver.com>
2024-08-28 15:36:59 +0900

    1

---
+++ src/page/Home.js
@@ -0,0 +1,21 @@
+//함수형 컴포넌트
+import React from "react";
+
+function Home() {
+  return <h1>Home 화면입니다.</h1>;
+}
+
+export default Home;
+
+//클래스 컴포넌트
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render(){
+//         return <h1>Home 화면입니다.</h1>
+//     }
+// }
+
+// export default Home;
+
+// 최근에 함수형 컴포넌트로 많이 개발하는 추세
