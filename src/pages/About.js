305bfa9666c00694c37052bac3fe1ce0d1db76d0
mminsong <pamten@naver.com>
2024-08-29 09:25:30 +0900

    https://www.youtube.com/watch?v=6GECT2Jrr_g 강의 끝

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About() {
+  return <h1>About 화면입니다.</h1>;
+}
+
+export default About;
+
+//const About = () => {
+//강사님이 화살표함수 좋아해서 화살표 함수로 바꿨지만 난 바꾸지 않음
---
+++ src/pages/List.js
@@ -0,0 +1,34 @@
+import React from "react";
+const User = ({ userData }) => {
+  return (
+    <tr>
+      <td>{userData.name}</td>
+      <td>{userData.email}</td>
+    </tr>
+  );
+};
+const UserList = () => {
+  const users = [
+    { email: "user1@gmail.com", name: "ㅁㅁㅁ" },
+    { email: "user2@gmail.com", name: "ㅅㅅㅅ" },
+    { email: "user3@gmail.com", name: "ㄴㄴㄴ" },
+    { email: "user4@gmail.com", name: "ㅈㅈㅈ" },
+  ];
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>이름</th>
+          <th>이메일</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <User userData={user} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
